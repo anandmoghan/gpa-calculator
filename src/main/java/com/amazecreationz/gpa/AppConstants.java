fed561bae9d6c807da97a30e9685cc816c147c38
Anand Mohan <moghan.anand@gmail.com>
2016-05-21 02:45:30 +0530

    maven

--- src/main/java/com/amazecreationz/gpa/AppConstants.java
+++ src/main/java/com/amazecreationz/gpa/AppConstants.java
@@ -2,5 +2,5 @@
 package com.amazecreationz.gpa;
 
 public interface AppConstants {
-	enum semesterKeys{ S1, S2, S3, S4, S5, S6, S7, S8, S9, S10};
+	enum semesterKeys{ S1, S2, S3, S4, S5, S6, S7, S8, S9};
 }
